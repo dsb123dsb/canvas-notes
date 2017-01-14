9ff4359123e4b46d61090abdec310b6065c289b4
zhouyonghui <zyh@treeholeapp.cn>
2017-01-14 18:10:10 +0800

    canvas-compositing animation

--- canvas-textpicTransform.js
+++ canvas-textpicTransform.js
@@ -17,182 +17,236 @@
   ctx6=canvas6.getContext("2d"),
   ctx7=canvas7.getContext("2d"),
   ctx8=canvas8.getContext("2d");
-  //绘制文本。fillText(tetx,x,y,[,maxWidth]),strokeText(tetx,x,y,[,maxWidth])
-  ctx.font = "32px serif";
-  ctx.textBaseline="middle"
-  ctx.strokeText("Hello ", 10, 50);
-  ctx.font = "16px serif";
-  ctx.strokeText("zeep", 100, 50);
-
-  //文本测量
-  var text=ctx.measureText("foo");
-  console.log(text.width);//16
-  //使用图片，先获取图片(很多方法)，在绘制图片drawImage(image,x,y)
-  //
-  var img=new Image();
-  img.onload=()=>{//要等到img.src加载在会执行
-      ctx1.drawImage(img,0,0);
-      ctx1.beginPath();
-      ctx1.moveTo(30,96);
-      ctx1.lineTo(70,66);
-      ctx1.lineTo(103,76);
-      ctx1.lineTo(170,15);
-      ctx1.stroke();
-    }
-   img.src="http://img1.3lian.com/2016/gif/w/15/61.jpg"  ;
-   //缩放Scalling，drawImage(image, x, y, width, height)
-   var img1=new Image();
-   img1.onload=()=>{
-      for(var i=0;i<3;i++){
-        for(var j=0;j<4;j++){
-          ctx2.drawImage(img1,i*50,38*j,50,37.5)
-        }
-      }
-   }
-   img1.src='https://mdn.mozillademos.org/files/5397/rhino.jpg';
-  //切片Slicing,drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
-  //sx、sy裁剪图片起始位置，swidth、sHeight裁剪图片宽高，dx、dy放置在画布中起始位置，dWidth、dHeight放缩后大小
-  //变形，ctx.save(),ctx.restore();
-  ctx3.fillRect(0,0,150,150);   // 使用默认设置绘制一个矩形
-  ctx3.save();                  // 保存默认状态
-
-  ctx3.fillStyle = '#09F'       // 在原有配置基础上做改变
-  ctx3.fillRect(15,15,120,120); // 使用新的设置绘制一个矩形
-
-  ctx3.save();                  // 保存当前状态
-  ctx3.fillStyle = '#FFF'       // 再次改变配置
-  ctx3.globalAlpha = 0.5;    
-  ctx3.fillRect(30,30,90,90);   // 使用新的配置绘制一个矩形
-
-  ctx3.restore();               // 重新加载之前的状态
-  ctx3.fillRect(45,45,60,60);   // 使用上一次的配置绘制一个矩形
-
-  ctx3.restore();               // 加载默认配置
-  ctx3.fillRect(60,60,30,30);   // 使用加载的配置绘制一个矩形  
-  //移动transtlating，translate(x, y)，左右偏移和上下偏移, canvas 和它的原点到一个不同的位置。
-  //做变形前先保存状态
-  function drawSpirograph(ctx,R,r,O){//绘制螺旋函数
-  var x1 = R-O;
-  var y1 = 0;
-  var i  = 1;
+  //组合，compositing,globalCompositeOperation=type(12种)
+  //裁剪路径Clipping paths，clip();
+  ctx.fillRect(0,0,150,150);
+  ctx.translate(75,75);
+  //create a circular clipping path
   ctx.beginPath();
-  ctx.moveTo(x1,y1);
-  do {
-    if (i>20000) break;
-    var x2 = (R+r)*Math.cos(i*Math.PI/72) - (r+O)*Math.cos(((R+r)/r)*(i*Math.PI/72))
-    var y2 = (R+r)*Math.sin(i*Math.PI/72) - (r+O)*Math.sin(((R+r)/r)*(i*Math.PI/72))
-    ctx.lineTo(x2,y2);
-    x1 = x2;
-    y1 = y2;
-    i++;
-  } while (x2 != R-O && y2 != 0 );
-  ctx.stroke();
-}
-function draw() {
-  ctx4.fillRect(0,0,150,150);
-  for (var i=0;i<3;i++) {
-    for (var j=0;j<3;j++) {
-      ctx4.save();
-      ctx4.strokeStyle = "#9CFF00";
-      ctx4.translate(25+j*50,25+i*50);
-      drawSpirograph(ctx4,10*(j+2)/(j+1),-4*(i+3)/(i+1),5);
-      ctx4.restore();
-    }
+  ctx.arc(0,0,60,0,Math.PI*2,true);
+  ctx.clip();
+  //draw background
+  var lingrad=ctx.createLinearGradient(0,-75,0,75);
+  lingrad.addColorStop(0,'#232256');
+  lingrad.addColorStop(0,'#143778');
+  ctx.fillStyle=lingrad;
+  ctx.fillRect(-75,-75,150,150);
+  //draw stars
+  for(var j=1;j<50;j++){
+    ctx.save();
+    ctx.fillStyle='#fff';
+    ctx.translate(75-Math.floor(Math.random()*150),
+                  75-Math.floor(Math.random()*150));
+    drawStar(ctx,Math.floor(Math.random()*4)+2);
+    ctx.restore();
   }
-}
-draw();
-  //罗旋圆
-  var r = 1;    
-  var draw1 = setInterval(function(){   
-    drawLine(ctx,r=r+1/r);  
-    },1);   
-    function drawLine(ct,r){    
-      ct.beginPath(); 
-      ct.arc(75,105,r,r,r+1); 
-      ct.stroke();  
-      if(r>40){
-        clearInterval(draw1);
-      } 
+  //封装创建星星函数
+  function drawStar(ctx,r){
+    ctx.save();
+    ctx.beginPath();
+    ctx.moveTo(r,0);
+    for(var i=0;i<9;i++){
+      ctx.rotate(Math.PI/5);
+      if(i%2 == 0) {
+        ctx.lineTo((r/0.525731)*0.200811,0);
+      } else {
+        ctx.lineTo(r,0);      
+      }
     }
-  //旋转，Rotating,原点为中心旋转 canvas。rotate(angle),顺时针
-  ctx5.translate(75,75);
-  for(var i=1;i<6;i++){
-    ctx5.save();
-    ctx5.fillStyle='rgb('+(51*i)+','+(255-51*i)+',255)';
-    for(var j=0;j<i*6;j++){//画每个环的圆点
-      ctx5.rotate(Math.PI*2/(i*6));
-      ctx5.beginPath();
-      ctx5.arc(0,i*12.5,5,5,0,Math.PI*2,true);
-      ctx5.fill();
+    ctx.closePath();
+    ctx.fill();
+    ctx.restore();
+  }
+  //基本动画Animation 1.清空 canvas 2.保存 canvas 状态 3.绘制动画图形（animated shapes）4.恢复 canvas 状态
+  //requestAnimationFrame(callback)告诉浏览器你希望执行一个动画，并在重绘之前，请求浏览器执行一个特定的函数来更新动画。
+  //动画太阳系
+  var sun=new Image();
+  var moon=new Image();
+  var earth=new Image();
+  function init(){
+    sun.src='https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
+    moon.src='https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
+    earth.src='https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
+    window.requestAnimationFrame(draw);
+  }
+  function draw(){
+    ctx1.globalCompositeOperation='destination-over';
+    ctx1.clearRect(0,0,150,150);
+    ctx1.fillStyle='rgba(0,0,0,0.4)';
+    ctx1.strokeStyle='rgba(0,153,255,0.4)';
+    ctx1.save();
+    ctx1.translate(75,75);
+    //earth
+    var time =new Date();
+    ctx1.rotate(((2*Math.PI)/60)*time.getSeconds()+((2*Math.PI)/60000)*time.getMilliseconds());
+    ctx1.translate(52.5,0);//轨道位置
+    ctx1.fillRect(0,-6,25,12);//shadow
+    ctx1.save();
+    ctx1.scale(0.5,0.5);//实例是300*300，这里要缩放图片,坐标也会相应缩放
+    ctx1.drawImage(earth,-12,-12);
+    ctx1.restore();
+    //moon
+    ctx1.save();
+    ctx1.rotate( ((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds() );
+    ctx1.translate(0,14.25);
+    ctx1.save();
+    ctx1.scale(0.5,0.5);//实例是300*300，这里要缩放图片
+    ctx1.drawImage(moon,-3.5,-3.5);
+    ctx1.restore();
+    ctx1.restore();
+
+    ctx1.restore();
+    ctx1.beginPath();
+    ctx1.arc(75,75,52.5,0,Math.PI*2,false);//Earth orbit
+    ctx1.stroke();
+    ctx1.drawImage(sun,0,0,150,150);//sun
+    window.requestAnimationFrame(draw);
+    console.log('1')
+  }
+  init();
+  //时钟动画
+  function clock(){
+    var now=new Date();
+    ctx2.save();
+    ctx2.clearRect(0,0,150,150);
+    ctx2.translate(75,75);
+    ctx2.scale(0.4,0.4);
+    ctx2.rotate(-Math.PI/2);
+    ctx2.strokeStyle='black';
+    ctx2.fillStyle='white';
+    ctx2.lineWidth=8;
+    ctx2.lineCap='round';
+    //hour marks
+     ctx2.save();
+    for (var i=0;i<12;i++){
+      ctx2.beginPath();
+      ctx2.rotate(Math.PI/6);
+      ctx2.moveTo(100,0);
+      ctx2.lineTo(120,0);
+      ctx2.stroke();
     }
-    ctx5.restore();
+    ctx2.restore();   
+    //minute marks
+    ctx2.save();
+    ctx2.linewidth=4;
+    for(var i=0;i<60;i++){
+      if(!i%5==0){
+        ctx2.beginPath();
+        ctx2.moveTo(117,0);
+        ctx2.lineTo(120,0);
+        ctx2.stroke();
+      }
+      ctx2.rotate(Math.PI/30);
+    }
+    ctx2.restore();
+    
+    var sec=now.getSeconds(),
+    min=now.getMinutes(),
+    hr=now.getHours();
+    hr=hr>=12?hr-12:hr;
+    ctx2.fillStyle='black';
+    //write Hour
+    ctx2.save();
+    ctx2.rotate( hr*(Math.PI/6) + (Math.PI/360)*min + (Math.PI/21600)*sec )
+    ctx2.lineWidth = 14;
+    ctx2.beginPath();
+    ctx2.moveTo(-20,0);
+    ctx2.lineTo(80,0);
+    ctx2.stroke();
+    ctx2.restore();    
+    // write Minutes
+    ctx2.save();
+    ctx2.rotate( (Math.PI/30)*min + (Math.PI/1800)*sec )
+    ctx2.lineWidth = 10;
+    ctx2.beginPath();
+    ctx2.moveTo(-28,0);
+    ctx2.lineTo(112,0);
+    ctx2.stroke();
+    ctx2.restore();
+    //write seconds
+    ctx2.save();
+    ctx2.rotate((Math.PI/30)*sec);
+    ctx2.strokeStyle = "#D40000";
+    ctx2.fillStyle = "#D40000";
+    ctx2.lineWidth=6;
+    ctx2.beginPath();
+    ctx2.moveTo(-30,0);
+    ctx2.lineTo(83,0);
+    ctx2.stroke();
+    ctx2.beginPath();
+    ctx2.arc(0,0,10,0,Math.PI*2,true);
+    ctx2.fill();
+    ctx2.beginPath();
+    ctx2.fillStyle = "rgba(0,0,0,1)";
+    ctx2.arc(0,0,3,0,Math.PI*2,true);
+    ctx2.fill();
+    ctx2.beginPath();
+    ctx2.arc(95,0,10,0,Math.PI*2,true);
+    ctx2.stroke();
+    ctx2.restore();
+    ctx2.restore();
+    window.requestAnimationFrame(clock);   
   }
-  //缩放Scalling,scale(x, y)x,y 分别是横轴和纵轴的缩放因子，它们都必须是正值。值比 1.0 小表示缩小，比 1.0 大则表示放大，值为 1.0 时什么效果都没有。
-  ctx6.strokeStyle = "#0cf";
-  ctx6.lineWidth = 1.5;
-  ctx6.fillRect(0,0,150,150);
+  window.requestAnimationFrame(clock);
+    //循环全景照片
+    var img = new Image();
 
-  //1. Uniform scaling
-  ctx6.save()
-  ctx6.translate(25,25);
-  drawSpirograph(ctx6,11,3,2.5);  // no scaling
+    // User Variables - customize these to change the image being scrolled, its
+    // direction, and the speed.
 
-  ctx6.translate(50,0);//在之前移动的基础上继续移动
-  ctx6.scale(0.75,0.75);
-  drawSpirograph(ctx6,11,3,2.5);
+    img.src = 'https://mdn.mozillademos.org/files/4553/Capitan_Meadows,_Yosemite_National_Park.jpg';
+    var CanvasXSize = 300;
+    var CanvasYSize = 200;
+    var speed = 30; //lower is faster
+    var scale = 1.05;
+    var y = -4.5; //vertical offset
 
-  ctx6.translate(65,0);
-  ctx6.scale(0.75,0.75);
-  drawSpirograph(ctx6,11,3,2.5);
-  ctx6.restore();
-  // 2.None-Uniform scaling(y direction)
-  ctx6.strokeStyle = "#fc0";
-  ctx6.save()
-  ctx6.translate(25,75);
-  drawSpirograph(ctx6,11,3,2.5);  // no scaling
+    // Main program
 
-  ctx6.translate(50,0);//在之前移动的基础上继续移动
-  ctx6.scale(1,0.75);
-  drawSpirograph(ctx6,11,3,2.5);
+    var dx = 0.75;
+    var imgW;
+    var imgH;
+    var x = 0;
+    var clearX;
+    var clearY;
+    var ctx;
 
-  ctx6.translate(50,0);
-  ctx6.scale(1,0.75);
-  drawSpirograph(ctx6,11,3,2.5);
-  ctx6.restore();
-  // 3.None-Uniform scaling(x direction)
-  ctx6.strokeStyle = "#cf0";
-  ctx6.save()
-  ctx6.translate(25,125);
-  drawSpirograph(ctx6,11,3,2.5);  // no scaling
+    img.onload = function() {
+        imgW = img.width*scale;
+        imgH = img.height*scale;
+        if (imgW > CanvasXSize) { x = CanvasXSize-imgW; } // image larger than canvas
+        if (imgW > CanvasXSize) { clearX = imgW; } // image larger than canvas
+        else { clearX = CanvasXSize; }
+        if (imgH > CanvasYSize) { clearY = imgH; } // image larger than canvas
+        else { clearY = CanvasYSize; }
+        //Set Refresh Rate
+        return setInterval(draw1, speed);
+    }
 
-  ctx6.translate(50,0);//在之前移动的基础上继续移动
-  ctx6.scale(0.75,1);
-  drawSpirograph(ctx6,11,3,2.5);
-
-  ctx6.translate(70,0);
-  ctx6.scale(0.75,1);
-  drawSpirograph(ctx6,11,3,2.5);
-  ctx6.restore();
-  //变形Transforms,transform(m11, m12, m21, m22, dx, dy)
-  var sin = Math.sin(Math.PI/6);
-  var cos = Math.cos(Math.PI/6);
-  ctx7.translate(75, 75);
-  var c = 0;
-  for (var i=0; i <= 12; i++) {
-    c = Math.floor(255 / 12 * i);
-    ctx7.fillStyle = "rgb(" + c + "," + c + "," + c + ")";
-    ctx7.fillRect(0, 0, 50, 5);
-    ctx7.transform(cos, sin, -sin, cos, 0, 0);//顺时针旋转
-  }
+    function draw1() {
+        //Clear Canvas
+        ctx3.clearRect(0,0,clearX,clearY);
+        //If image is <= Canvas Size
+        if (imgW <= CanvasXSize) {
+            //reset, start from beginning
+            if (x > (CanvasXSize)) { x = 0; }
+            //draw aditional image
+            if (x > (CanvasXSize-imgW)) { ctx3.drawImage(img,x-CanvasXSize+1,y,imgW,imgH); }
+        }
+        //If image is > Canvas Size
+        else {
+            //reset, start from beginning
+            if (x > (CanvasXSize)) { x = CanvasXSize-imgW; }
+            //draw aditional image
+            if (x > (CanvasXSize-imgW)) { ctx3.drawImage(img,x-imgW+1,y,imgW,imgH); }
+        }
+        //draw image
+        ctx3.drawImage(img,x,y,imgW,imgH);
+        //amount to move
+        x += dx;
+    }
   
-  ctx7.setTransform(-1, 0, 0, 1, 75, 75);//重置为单位矩阵
-  ctx7.fillStyle = "rgba(255, 128, 255, 0.5)";
-  ctx7.fillRect(0, 25, 50, 50);
-
-
-
-
-
-
-
-}
+  
+  
+  
+  }
